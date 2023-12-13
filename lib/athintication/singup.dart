54337d0feda3688d6852c45878f206eb4b746e6e
Abir Hossain <abir2526@gmail.com>
2023-12-14 00:13:01 +0600

    Commit Message

--- lib/athintication/singup.dart
+++ lib/athintication/singup.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 class Singup extends StatefulWidget {
-   Singup({super.key});
+  Singup({super.key});
 
   @override
   State<Singup> createState() => _SingupState();
 }
 
 class _SingupState extends State<Singup> {
- final emailcontroler = TextEditingController();
+  final emailcontroler = TextEditingController();
   final paswordcotroler = TextEditingController();
 
   final namecotroler = TextEditingController();
@@ -29,31 +28,36 @@ class _SingupState extends State<Singup> {
               mainAxisAlignment: MainAxisAlignment.center,
               mainAxisSize: MainAxisSize.min,
               children: [
-                TextField(controller: emailcontroler,decoration: InputDecoration(hintText: "email")),
-                TextField(controller: paswordcotroler,decoration: InputDecoration(hintText: "passwword")),
-                  TextField(controller: namecotroler,decoration: InputDecoration(hintText: "name")),
-                    TextField(controller: agecotroler,decoration: InputDecoration(hintText: "age")),
-                      TextField(controller: thikanacotroler,decoration: InputDecoration(hintText: "city")),
-              
-                 ElevatedButton(onPressed: ()async{
-            
-            
-                   
-                   String? userid = await Singup();
-            
-                        if (userid != null) {
-                          Map<String, dynamic> data = {
-                            'name':
-                                   namecotroler.text,
-                            'age': agecotroler.text,
-                             'kihcana': thikanacotroler.text,
-                          };
-            
-                           await userdata (userid, data);
-            
-                        
-                        }
-                 }, child: Text("singin"))
+                TextField(
+                    controller: emailcontroler,
+                    decoration: InputDecoration(hintText: "email")),
+                TextField(
+                    controller: paswordcotroler,
+                    decoration: InputDecoration(hintText: "passwword")),
+                TextField(
+                    controller: namecotroler,
+                    decoration: InputDecoration(hintText: "name")),
+                TextField(
+                    controller: agecotroler,
+                    decoration: InputDecoration(hintText: "age")),
+                TextField(
+                    controller: thikanacotroler,
+                    decoration: InputDecoration(hintText: "city")),
+                ElevatedButton(
+                    onPressed: () async {
+                      String? userid = await Singup();
+
+                      if (userid != null) {
+                        Map<String, dynamic> data = {
+                          'name': namecotroler.text,
+                          'age': agecotroler.text,
+                          'kihcana': thikanacotroler.text,
+                        };
+
+                        await userdata(userid, data);
+                      }
+                    },
+                    child: Text("singin"))
               ],
             ),
           ),
@@ -62,23 +66,21 @@ class _SingupState extends State<Singup> {
     );
   }
 
-    // Future phonnumberathitication ()async{
-    //   FirebaseAuth.instance.verifyPhoneNumber(phoneNumber:phonenumber.text,verificationCompleted:(_){}, verificationFailed:(e){}, codeSent:(String verificationId , int ? token ){
-    //       Navigator.push(context, MaterialPageRoute(builder: (context) =>Codepage(code:verificationId,),));
-    //   }, codeAutoRetrievalTimeout:(e){});
-    // }
+  // Future phonnumberathitication ()async{
+  //   FirebaseAuth.instance.verifyPhoneNumber(phoneNumber:phonenumber.text,verificationCompleted:(_){}, verificationFailed:(e){}, codeSent:(String verificationId , int ? token ){
+  //       Navigator.push(context, MaterialPageRoute(builder: (context) =>Codepage(code:verificationId,),));
+  //   }, codeAutoRetrievalTimeout:(e){});
+  // }
 
+  Future Singup() async {
+    var userinfo = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailcontroler.text.trim(),
+        password: paswordcotroler.text.trim());
 
-    Future Singup ()async{
-  var userinfo  = await FirebaseAuth.instance.createUserWithEmailAndPassword(email:emailcontroler.text, password:paswordcotroler.text);
+    return userinfo.user!.uid;
+  }
 
-   return userinfo.user!.uid;
-    }
-
-
-    Future userdata ( String uid , Map<String,dynamic> data)async{
-    await  FirebaseFirestore.instance.collection("user_data").doc(uid).set(data);
-    }
-
-   
+  Future userdata(String uid, Map<String, dynamic> data) async {
+    await FirebaseFirestore.instance.collection("user_data").doc(uid).set(data);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
 
-
 import 'package:massenger/athintication/redairedpage.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
-
-
-void main()async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
- class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:Redairegpage(),
+      home: Redairegpage(),
     );
   }
 }
